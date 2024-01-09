8b82358ce7ca6b7f928cea1d4687fb9a1ac0ff30
fenil09 <backsystem9909@gmail.com>
2024-01-09 11:37:30 -0800

    Added a Text Composable which helps in Enhancing the UI

--- app/src/main/java/com/base/starbucks_ui_clone/Home.kt
+++ app/src/main/java/com/base/starbucks_ui_clone/Home.kt
@@ -1,5 +1,6 @@
 package com.base.starbucks_ui_clone
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,8 +17,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.base.starbucks_ui_clone.ui.theme.Starbucks_UI_CloneTheme
 
 class Home : ComponentActivity() {
@@ -28,10 +32,18 @@ class Home : ComponentActivity() {
 
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
                         .background(Color.White)
                 ) {
                     TopSection()
+                    Spacer(modifier = Modifier.height(50.dp))
+                    Text(text = "Our Way Of" + "\n" + "Loving You Back",
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+                        modifier = Modifier.padding(end = 135.dp)
+                    )
                 }
             }
         }
@@ -44,8 +56,9 @@ fun TopSection(
 ){
     Row(
         horizontalArrangement = Arrangement.SpaceAround,
-        modifier = modifier.fillMaxWidth()
-            .padding(top=35.dp)
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 35.dp)
 
 
     ){
